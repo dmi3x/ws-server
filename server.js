cb8d9d99b5387c4a28f3daa28d55650d3eb29ada
dmi3x <553046+dmi3x@users.noreply.github.com>
2023-01-26 21:07:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
 
 const cors = require('cors');
-io.use(cors());
-
-app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Forbidden')
